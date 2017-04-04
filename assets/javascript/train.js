38749bbe1a870385eba15e746307be92f1a4f4d0
Janet Walker <janetwalker@Janets-MBP.attlocal.net>
2017-04-04 13:16:02 -0400

    Database and table fields are populating

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -7,7 +7,7 @@ var config = {
 	messagingSenderId: "332307435243"
 	};
 
-firebase.initializeApp(config);
+	firebase.initializeApp(config);
 
 	var database = firebase.database();
 
@@ -19,18 +19,62 @@ firebase.initializeApp(config);
 $("#add-train").on("click", function(event){
 	event.preventDefault();
 
-	train = ("#train-input").val().trim();
-	destination = ("#dest-input").val().trim();
-	firstTime = ("#time-input").val().trim();
-	frequency = ("#freq-input").val().trim();
+	train = $("#train-input").val().trim();
+	destination = $("#dest-input").val().trim();
+	firstTime = $("#time-input").val().trim();
+	frequency = $("#freq-input").val().trim();
 
-	dataRef.ref().push({
+	var newTrain = {
 		train: train,
 		destination: destination,
 		firstTime: firstTime,
 		frequency: frequency,
-		dateAdded: firebase.database.ServerValue.TIMESTAMP
-	})
+	};
+
+	database.ref().push(newTrain);
+
+	console.log(newTrain.train);
+	console.log(newTrain.destination);
+	console.log(newTrain.firstTime);
+	console.log(newTrain.frequency);
+
+	alert("Train Schedule Successfully Added");
+
+	$("#train-input").val("");
+	$("#dest-input").val("");
+	$("#time-input").val("");
+	$("#freq-input").val("");
+
+  // Prevents moving to new page
+  return false;
 
 });
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+	console.log(childSnapshot.val());
+
+	train = childSnapshot.val().train;
+	destination = childSnapshot.val().destination;
+	firstTime = childSnapshot.val().firstTime;
+	frequency = childSnapshot.val().frequency;
+
+  	console.log(train);
+  	console.log(destination);
+  	console.log(firstTime);
+ 	console.log(frequency);
+
+ 	var trainTimeFormat = moment.unix(firstTime).format("MM/DD/YY");
+
+ 	var nextArrival = "";
+
+ 	var minAway = "";
+
+    $("#train-table > tbody").append("<tr><td>" + train + "</td><td>" + destination + "</td><td>" +
+  trainTimeFormat + "</td><td>" + nextArrival + "</td><td>" + minAway + "</td></tr>");
+
+});
+
+ 
+
 
